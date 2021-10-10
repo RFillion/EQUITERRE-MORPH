ba101c9662c0a5970430ca561ac8c6527d26a6ba
Raphaël Fillion <raph.fillion18@gmail.com>
2021-10-10 15:06:32 -0400

    Navbar --done
    Navbar mobile and desktop done

--- EQUITERRE-MORPH/main.js
+++ EQUITERRE-MORPH/main.js
@@ -1,3 +1,17 @@
 import './main.scss'
 
+//NavBar BTN
+let NavBtn = document.querySelector('.menu__toggle a');
+let Hamburger = document.querySelector('.hamburger');
+
+NavBtn.addEventListener('click', () => {
+    Hamburger.style.display = 'block';
+    Hamburger.classList.toggle('open');
+    if (!Hamburger.classList.contains('open')) {
+        Hamburger.style.display = 'none';
+    }
+});
+
+//
+
 
