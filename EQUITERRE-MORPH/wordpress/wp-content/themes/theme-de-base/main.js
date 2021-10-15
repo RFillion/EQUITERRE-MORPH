e8f4d81fc23a1b33794546b0377e15b20c6ce0fc
Raphaël Fillion <raph.fillion18@gmail.com>
2021-10-15 13:47:15 -0400

    ajout du js dans wordpress

--- EQUITERRE-MORPH/wordpress/wp-content/themes/theme-de-base/main.js
+++ EQUITERRE-MORPH/wordpress/wp-content/themes/theme-de-base/main.js
@@ -1,1 +1,54 @@
 /* Wow, tellement un beau spot pour écrire du JS */
+import './main.scss'
+
+//NavBar BTN
+let NavBtn = document.querySelector('.menu__toggle a');
+let Hamburger = document.querySelector('.hamburger');
+
+NavBtn.addEventListener('click', () => {
+    Hamburger.style.display = 'block';
+    Hamburger.classList.toggle('open');
+    if (!Hamburger.classList.contains('open')) {
+        Hamburger.style.display = 'none';
+    }
+});
+
+
+//NavBar Scroll
+let Nav = document.querySelector('.Nav');
+
+window.onscroll = () => {
+    "use strict";
+    if(document.body.scrollTop >= 100 | document.documentElement.scrollTop >= 100) {
+        Nav.classList.add('colored');
+        Nav.classList.remove('transparent');
+    } else {
+        Nav.classList.add('transparent');
+        Nav.classList.remove('colored');
+    }
+};
+
+
+//SERVICE
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50
+      }
+    }
+  });
