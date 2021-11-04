018c4c6274f71335755b02772f55d93850143e8a
Raphaël Fillion <raph.fillion18@gmail.com>
2021-11-03 20:19:22 -0400

    404

--- wordpress/wp-content/themes/theme-de-base/partials/404.php
+++ wordpress/wp-content/themes/theme-de-base/partials/404.php
@@ -1,4 +1,10 @@
-<section>
-  <h2>404</h2>
-	<img src="https://media.giphy.com/media/3HLMNi9U0yb4CDldVO/giphy.gif" alt="404">
-</section>
+<div class="boite404">
+  <div class="oops">
+    <h1>Ohoh! Erreur 404!</h1>
+    <p>La page demandée n’a pu être trouvée.</p>
+  </div>
+  <div class="animation404">
+    <img class="terre" src="/wp-content/themes/theme-de-base/sources\medias\img_animation\terre-equiterre2.png" alt="">
+    <img class="interrogation" src="/wp-content/themes/theme-de-base/sources\medias\img_animation\interrogation.png" alt="">
+  </div>
+</div>
